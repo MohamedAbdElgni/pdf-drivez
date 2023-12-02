2ce8ad5066752576a7a294bc1141b03ff3a5ad68
sok <mohamed.kk878@gmail.com>
2023-12-02 18:46:39 +0200

    search

--- pdf_d/forms.py
+++ pdf_d/forms.py
@@ -4,11 +4,9 @@ from wtforms import BooleanField, SelectField, SubmitField
 
 class Filters(FlaskForm):
     exact_match = BooleanField('Exact Match', default=False, id='match-filter')
-    year_select = SelectField('Year', choices=["Year", 1990, 1995,
-                                               2000, 2005, 2010,
-                                               2015, 2022],default="Year",id='year-filter',)
+    year_select = SelectField('Year', choices = ["Year", *[int(x) for x in range(1995, 2023)]])
     pages_select = SelectField('Pages', choices=[
-                               "Pages", 100, 200, 300, 400, 500], default=500, id='pages-filter')
+                               "Pages", 100, 200, 300, 400, 500 ,600,700,800,900,1000,1500,1800,2000], default=500, id='pages-filter')
     submit = SubmitField('Filter', id='submit')
     
     
